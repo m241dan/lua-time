92608712b2fe7874caeaa443e188e053ff52c696
Davenge <korisd@gmail.com>
2015-10-08 15:55:21 -0400

    made the time.c compatible

--- time.c
+++ time.c
@@ -37,10 +37,9 @@ const UINT USEC = 	8;
 
 /* time helper function */
 double get_time( UINT k ){
-	
 	struct timeval tv;
 	gettimeofday( &tv, NULL );
-	
+
 	if( k==SEC ) 		return tv.tv_sec;
 	else if( k==MSEC )	return (tv.tv_sec + (double)((int)(tv.tv_usec*0.001) * 0.001));
 	else if( k==USEC )	return (tv.tv_usec*0.000001);
@@ -49,49 +48,43 @@ double get_time( UINT k ){
 
 /* get miliseconds relative to seconds since EPOCH */
 int t_mili (lua_State *L) {
-	
 	lua_pushnumber(L, get_time( MSEC ) );
 	return 1;
 }
 
 /* get seconds since EPOCH */
 int t_seconds (lua_State *L) {
-	
 	lua_pushnumber( L, get_time( SEC ) );
 	return 1;
 }
 
 /* get microseconds relative to seconds since EPOCH */
 int t_micro (lua_State *L) {
-	
 	lua_pushnumber(L, get_time( USEC ) );
 	return 1;
 }
 
 /* return the diference in miliseconds relative to seconds since EPOCH */
 int t_diff (lua_State *L){
-	
 	double v1= (double)luaL_checknumber( L, 1 );
-	
+
 	lua_pushnumber( L, ( get_time( MSEC ) - v1 ) );
 	return 1;
 }
 
 /* return seconds, miliseconds and microseconds */
 int t_time (lua_State *L){
-	
 	struct timeval tv;
 	gettimeofday( &tv, NULL );
-	
+
 	lua_pushnumber( L, tv.tv_sec );
 	lua_pushnumber( L, (double)((int)(tv.tv_usec*0.001) * 0.001) );
 	lua_pushnumber( L, (double)(tv.tv_usec * 0.000001) );
-	
 	return 3;
 }
 
 /* register functions */
-const struct luaL_reg time_lib[] = {
+const struct luaL_Reg time_lib[] = {
   {"getMiliseconds", t_mili},
   {"getSeconds", t_seconds},
   {"getMicroseconds", t_micro},
@@ -103,6 +96,6 @@ const struct luaL_reg time_lib[] = {
 /* register lib */
 LUALIB_API int luaopen_time (lua_State *L) {
   
-  luaL_register(L, "time", time_lib);
+  luaL_newlib(L, time_lib);
   return 1;
 }
